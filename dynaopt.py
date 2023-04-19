2ecbc2836b4fad92955976164293fffe59b85c2b
Cindy Huang <huang.cindy@hotmail.com>
2023-04-19 10:14:25 -0400

    clean up optimizer

--- dynaopt.py
+++ dynaopt.py
@@ -1,6 +1,7 @@
 import torch
 from functools import reduce
 from copy import deepcopy
+import math
 
 
 class DynamicOptimizer(torch.optim.Optimizer):
@@ -20,32 +21,12 @@ class DynamicOptimizer(torch.optim.Optimizer):
         defaults = dict(lr=lr, debug=debug, lr_choices=lr_choices + [lr])
         super(DynamicOptimizer, self).__init__(params, defaults)
         self._params = self.param_groups[0]['params']
-        self._numel_cache = None
         self.eval_count = 0
 
-    def _numel(self):
-        """
-        Calculate the number of elements in params. From: https://github.com/nlesc-dirac/pytorch/blob/124a693b713c58bb6475db53090d4a5c83194653/lbfgsnew.py
-        """
-        if self._numel_cache is None:
-            self._numel_cache = reduce(lambda total, p: total + p.numel(), self._params, 0)
-        return self._numel_cache
-
-    # def _make_copy(self):
-    #     """
-    #     Make a deep copy of self._params. From: https://github.com/nlesc-dirac/pytorch/blob/124a693b713c58bb6475db53090d4a5c83194653/lbfgsnew.py
-    #     """
-    #     offset = 0
-    #     new_params = []
-    #     for p in self._params:
-    #         numel = p.numel()
-    #         new_param1=p.data.clone().contiguous().view(-1)
-    #         offset += numel
-    #         new_params.append(new_param1)
-    #     assert offset == self._numel()
-    #     return torch.cat(new_params,0)
-
     def _make_copy(self):
+        """
+        Make a deep copy of self._params.
+        """
         current_params = []
         for param in self._params:
             current_params.append(deepcopy(param.data))
@@ -53,39 +34,34 @@ class DynamicOptimizer(torch.optim.Optimizer):
 
     def _reset_params(self, new_params):
         """
-        Reset self._params with newparams. From: https://github.com/nlesc-dirac/pytorch/blob/124a693b713c58bb6475db53090d4a5c83194653/lbfgsnew.py
+        Reset self._params with newparams. 
+        Adapted from: https://github.com/nlesc-dirac/pytorch/blob/124a693b713c58bb6475db53090d4a5c83194653/lbfgsnew.py
         """
-        offset = 0
-        # for p in self._params:
-        #     numel = p.numel()
-        #     p.data.copy_(new_params[offset:offset+numel].view_as(p.data))
-        #     offset += numel
-        # assert offset == self._numel()
         for p, p_current in zip(self._params, new_params):
             p.data.copy_(p_current)
-            # offset += numel
-        # assert offset == self._numel()
 
     def _try_update(self, lr, curr_params, curr_grad):
         """
         Try an update for self._params using learning rate lr,
         current parameters and gradient curr_params and curr_grad.
         """
-        zipped = zip(self._params, curr_params, curr_grad)
-        for p_next, p_current, g_current in zipped:
-            p_next.data = p_current - lr * g_current
+        # p.data -= g_current*lr
+        for p, g_current in zip(self._params, curr_grad):
+            p.data.sub_(g_current.data*lr)
 
-    def _search_lr(self, closure, curr_params, curr_grad, curr_lr, lr_choices, debug):
+    def _search_lr(self, closure, curr_params, curr_grad, lr_choices, debug):
         """
         Return the lr that gives the lowest loss out of all learning rates in 
         lr_choices.
         """
         # initialize
-        best_lr = curr_lr
-        best_loss = float(closure())
+        best_lr = -1
+        best_loss = math.inf
 
         # try each lr choice
         for choice in lr_choices:
+            # reset params
+            self._reset_params(curr_params)
             # update params
             self._try_update(choice, curr_params, curr_grad)
             # get loss
@@ -93,8 +69,6 @@ class DynamicOptimizer(torch.optim.Optimizer):
             if curr_loss < best_loss:
                 best_lr = choice
                 best_loss = curr_loss
-            # reset params
-            self._reset_params(curr_params)
         
         return best_lr, best_loss
 
@@ -110,7 +84,6 @@ class DynamicOptimizer(torch.optim.Optimizer):
 
         # save relevant parameters
         group = self.param_groups[0]
-        lr = group['lr']
         debug = group['debug']
         lr_choices = group['lr_choices']
 
@@ -120,14 +93,17 @@ class DynamicOptimizer(torch.optim.Optimizer):
 
         # calculate best lr and associated val loss
         with torch.no_grad():
-            best_lr, best_loss = self._search_lr(closure, curr_params, curr_grad, lr, lr_choices, debug)
-        
+            best_lr, best_loss = self._search_lr(closure, curr_params, curr_grad, lr_choices, debug)
+
+        # reset before updating lr and params
+        self._reset_params(curr_params)
+        self._try_update(best_lr, curr_params, curr_grad)
+
         if debug:
             self.eval_count += 1
             if self.eval_count % 80 == 79:
                 print("Found best lr {} for batch {}.".format(best_lr, self.eval_count))
-
-        # update lr and params
-        self._try_update(best_lr, curr_params, curr_grad)
+            # print(self._params[0].shape, curr_params[0].shape)
+            # print("Are equal: ", torch.eq(self._params[0], curr_params[0]))
 
         return best_loss
